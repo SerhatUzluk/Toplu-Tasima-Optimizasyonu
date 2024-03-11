504cd6d7ae7ddb90bfe3cadd69fcd9d9351a1538
Serhat Uzluk <132030068@ogr.uludag.edu.tr>
2024-03-11 10:00:25 +0300

    Firebase hatası giderildi.

--- src/script.js
+++ src/script.js
@@ -2,13 +2,13 @@ import {initializeApp} from "firebase/app"
 import { getFirestore } from "firebase/firestore";
 import { useEffect } from "react";
 const firebaseConfig = {
-    apiKey: "AIzaSyCuOTRSpfvvdtI1sR_fluKz3btIvbUiZhQ",
-    authDomain: "toplutasimaoptimizasyonu.firebaseapp.com",
-    projectId: "toplutasimaoptimizasyonu",
-    storageBucket: "toplutasimaoptimizasyonu.appspot.com",
-    messagingSenderId: "397883387700",
-    appId: "1:397883387700:web:07902bfc4f3380c4f6bf61",
-    measurementId: "G-ZHXKGSYSPT"
+  apiKey: "AIzaSyDBCL8LJ50xTbW7UkvN6AKiPST0acdfY0Y",
+  authDomain: "travelingoptimization.firebaseapp.com",
+  projectId: "travelingoptimization",
+  storageBucket: "travelingoptimization.appspot.com",
+  messagingSenderId: "621128430008",
+  appId: "1:621128430008:web:71d457c489020cdb761f4d",
+  measurementId: "G-3KQJPJF7HM"
   };
 
   const app = initializeApp(firebaseConfig);
